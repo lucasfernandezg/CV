1a5e0247793531f83874b13e044df749b0d295d1
Lucas Fernandez Garabenta <lucasrafaelfg@gmail.com>
2022-02-10 00:33:24 -0300

    Almost Ready

---
+++ web_cv/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Estudios(models.Model):
+    institucion = models.CharField(max_length=32)
+    grado = models.CharField(max_length=32)
+    inicio = models.DateField(null=True)
+    fin = models.DateField(null=True)
+    descripcion = models.CharField(max_length=254)
+    i = models.IntegerField()
+
+
+class Programacion(models.Model):
+
+    lenguaje = models.CharField(max_length=32)
+    observacion = models.CharField(max_length=254)
+
+
+class Trabajos(models.Model):
+    institucion = models.CharField(max_length=32)
+    puesto = models.CharField(max_length=32)
+    inicio = models.DateField(null=True)
+    fin = models.DateField(null=True)
+    descripcion = models.CharField(max_length=254)
+    i = models.IntegerField()
---
+++ web_cv/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+# Ahora heredaremos TemplateView en vez de View.
+from django.views.generic import ListView, TemplateView
+from web_cv import models
+
+
+class Main_cv(TemplateView):
+    template_name = "web_cv/main_cv.html"
+
+    def get_context_data(self, **kwargs):
+      context = super().get_context_data(**kwargs)
+      context["estudios"] = list(models.Estudios.objects.all())
+      context["trabajos"] = list(models.Trabajos.objects.all())
+      return context
